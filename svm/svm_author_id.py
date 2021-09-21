31e58de521ce737f380028ddea8f0f8e7ff66cc4
Mateus Bazani <mrbazani@gmail.com>
2021-09-21 10:12:25 -0300

    Ends the SVM trainning

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -8,6 +8,7 @@
     Chris has label 1
 """
     
+from operator import xor
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -22,7 +23,38 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+# com kernel linear e todo o conjunto de dados, acuracia de 98,4%
+# clf = svm.SVC(kernel='linear')
+clf = svm.SVC(C=10000)
+
+""" features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)] """
+
+training_time = time()
+test_data = clf.fit(features_train, labels_train)
+print("Training time for SVC with rbf kernel: ", round(time() - training_time, 3), "s")
+
+predict_time = time()
+predict_data = clf.predict(features_test)
+print("Predict time for SVC with rbf kernel: ", round(time() - predict_time, 3), "s")
+
+cris_data = 0
+sarah_data = 0
+
+for data in predict_data:
+    if data == 1:
+        cris_data = cris_data + 1
+    else:
+        sarah_data = sarah_data + 1
+
+print("Cris e-mails: ", cris_data)
+print("Sarah e-mails: ", sarah_data)
+
+predict_accuracy = accuracy_score(predict_data, labels_test)
+print("My SVC accuracy was: ", predict_accuracy)
 
 #########################################################
 
