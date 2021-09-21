85961dbef1f896b03448e7bd35aec3ba9bf6afb7
Mateus Bazani <mrbazani@gmail.com>
2021-09-21 10:12:59 -0300

    Ends the decision tree trainning

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,13 +19,20 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
+print("Number of features in my training data : ", len(features_train[0]))
 
+clf = DecisionTreeClassifier(min_samples_split=40)
 
-#########################################################
-### your code goes here ###
+training_time = time()
+test_data = clf.fit(features_train, labels_train)
+print("Training time for Decision Tree: ", round(time() - training_time, 3), "s")
 
+predict_time = time()
+predict_data = clf.predict(features_test)
+print("Predict time for Decision Tree: ", round(time() - predict_time, 3), "s")
 
-#########################################################
-
-
+predict_accuracy = accuracy_score(predict_data, labels_test)
+print("My Decision Tree accuracy: ", predict_accuracy)
